41529813c38f42941c7389e83d8a0ff0f9157ba0
Mathieu Robin <mathieu.robin1@gmail.com>
2019-08-27 18:00:24 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/profile", to: 'pages#profile'
 
-  resources :flats, only: [:new, :show, :create, :edit, :update, :delete]
+  resources :flats, only: [:new, :show, :create]
 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
