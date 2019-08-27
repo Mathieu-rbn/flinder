65e564c8eb2ea3ec7226ef49a72413b2288d8177
Mathieu Robin <mathieu.robin1@gmail.com>
2019-08-27 14:30:03 +0200

    done

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,4 +3,24 @@ class Flat < ApplicationRecord
   has_many :viewings
   has_many :matches
   has_many :flat_photos
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :property_type, presence: true
+
+  # validates :bathroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :street, presence: true
+  # validates :district, presence: true
+  # validates :city, presence: true
+  # validates :total_rent, presence: true
+  # validates :vacant_from, presence: true
+
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :property_type, presence: true
+
+  # validates :street, presence: true
+  # validates :district, presence: true
+  # validates :city, presence: true
+  # validates :total_rent, presence: true
+  # validates :bathroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :vacant_from, presence: true
+
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+  # validates :bedroom, presence: true
+
+  validates :exemple_a, presence: true
+  validates :exemple_b, uniqueness: true
+
 end
