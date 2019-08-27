c363a0f56285f3bcd914c9f4da2dade005025980
Mathieu Robin <mathieu.robin1@gmail.com>
2019-08-27 19:12:55 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   get "/profile", to: 'pages#profile'
 
   resources :flats, only: [:new, :show, :create]
-
-  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
