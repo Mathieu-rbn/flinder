e1fb99f0a297bfcd96265ffbd88e2537212aa40b
juanca limo <juancarlos.limo@hotmail.com>
2019-08-27 19:03:37 +0200

    user show option after the match. Point 8 TRello

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :property_type, presence: true
+  # validates :title, presence: true
+  # validates :description, presence: true
+  # validates :property_type, presence: true
 end
