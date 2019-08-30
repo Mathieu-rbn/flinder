48c9dad043ab81ec446e89e9099d721db02e01cb
Juergen Nguyen <juergen.nguyen@gmail.com>
2019-08-30 11:20:08 +0200

    add index page for displaying all flats

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,12 +10,17 @@ initHammer();
 const flats = document.querySelectorAll('.flat-box');
 
 const buttons = document.querySelectorAll('.like-btn');
-console.log(buttons)
+console.log("Lese den Button aus")
+// console.log(buttons)
 buttons.forEach((btn) => {
   btn.addEventListener('click', (event) => {
     document.querySelector('.flat-box.active').remove();
-    document.querySelectorAll('.flat-box')[0].classList.add('active')
-    const flatId = event.currentTarget.dataset.flatId
+    document.querySelectorAll('.flat-box')[0].classList.add('active');
+    const flatId = event.currentTarget.dataset.flatId;
+    const userId = event.currentTarget.dataset.userId;
+    console.log(flatId);
+    console.log (userId);
+    return fetch(`/flats`)
     // Call backend to create a viewing
     // fetch
     // => /flats/:id/viewings/new
