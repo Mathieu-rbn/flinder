fbfffa08209b9bdf9dd321f885f4f5db08258f8d
Mathieu Robin <mathieu.robin1@gmail.com>
2019-09-02 14:42:46 +0200

    added the bulk image uploader

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,17 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+    # @flats = @flats.where("district ILIKE ?", "%#{params[:query]}%") if params[:query].present?
+    # @flats = @flats.where(bedroom: params[:query1].to_i) if params[:query1].present?
+    # @flats = @flats.where("street ILIKE ?", "%#{params[:query2]}%") if params[:query2].present?
+  end
+
+  # GET /albums/1
+  # GET /albums/1.json
   def show
-    @flat = Flat.find(params[:id])
+    # @flat = Flat.find(params[:id])
     @markers = [{
       lat: @flat.latitude,
       lng: @flat.longitude,
@@ -16,42 +27,37 @@ class FlatsController < ApplicationController
     Flat.next
   end
 
-
-
-  def index
-    @flats = Flat.all
-    #@flats = Flat.all
-#    @flats = @flats.where("district ILIKE ?", "%#{params[:query]}%") if params[:query].present?
-#    @flats = @flats.where(bedroom: params[:query1].to_i) if params[:query1].present?
-#    @flats = @flats.where("street ILIKE ?", "%#{params[:query2]}%") if params[:query2].present?
-  end
-
-
   def create
     @flat = Flat.new(flat_params)
     @user = current_user
 
     @flat.user = @user
     @flat.save
+
+    create_photos(@flat)
     redirect_to flat_path(@flat)
     # redirect_to new_flat_path
   end
 
-  # def edit
-  #   @flat = Flat.find(params[:id])
-  # end
+     private
 
-  # def update
-  #   @flat = Flat.find(params[:id])
-  # end
-
-  # def delete
-  #   @flat = Flat.find(params[:id])
+  # Use callbacks to share common setup or constraints between actions.
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 
   def flat_params
     params.require(:flat).permit(:title, :description, :bathroom, :bedroom, :property_type, :street,
                                  :district, :post_code, :city, :price, :content, :total_rent,
                                  :garage, :level_floor, :floors_number, :vacant_from, :square_meter,
-                                 :land, :year_construction, :type_heating, :elevator, :photo)
+                                 :land, :year_construction, :type_heating, :elevator)
   end
+
+  def create_photos (resource)
+    images = params.dig(:flat, :photos) || []
+    images.each do |image|
+      resource.photos.create(photo: image)
+    end
+  end
+
 end
--- app/models/flat_photo.rb
+++
@@ -1,6 +0,0 @@
-class FlatPhoto < ApplicationRecord
-  belongs_to :flat
-  mount_uploader :data, PhotoUploader
-
-  validates :data, presence: true
-end
